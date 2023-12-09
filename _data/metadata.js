d81ce4437d7d9ac656923b358557f214f7d5d7fc
Umar Mahmud <ujmahmud@gmail.com>
2023-12-08 21:30:36 -0500

    updated site metadata, removed navigation

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,6 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "MidnightCoder",
+    url: "https://midnightcoder.dev",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
-	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
-	}
+	description: "A technical blog on Python, JavaScript, TypeScript, Scala, F# and functional programming"
 }
